beae3ad8ecf59a54b83bd0b2b89e11dc321dc966
pba34 <pba34@sfu.ca>
2022-04-04 21:51:58 -0700

    IP change to gatling

--- c756-exer/tools/gatling-1-music.sh
+++ c756-exer/tools/gatling-1-music.sh
@@ -3,7 +3,7 @@ docker container run --rm \
   -v ${PWD}/gatling/results:/opt/gatling/results \
   -v ${PWD}/gatling:/opt/gatling/user-files \
   -v ${PWD}/gatling/target:/opt/gatling/target \
-  -e CLUSTER_IP='34.83.42.29' \
+  -e CLUSTER_IP=`tools/getip.sh kubectl istio-system svc/istio-ingressgateway` \
   -e USERS=1 \
   -e SIM_NAME=WriteTableSim \
   --label gatling \
