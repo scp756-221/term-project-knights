0369adeb9ef81fee6d05ecc0ec0ba89f2e74699f
Shivek Chhabra <sca320@sfu.ca>
2022-04-08 13:52:04 -0700

    actions 4

--- c756-exer/leaderboard_local/test_app.py
+++ c756-exer/leaderboard_local/test_app.py
@@ -2,9 +2,9 @@ import requests
 
 
 def test_read():
-    assert True
-    # response = requests.get("http://localhost:5000/api/v1/leaderboard/")
-    # assert response.status_code == 200
+    # assert True
+    response = requests.get("http://localhost:5000/api/v1/leaderboard/")
+    assert response.status_code == 200
 
 def test_read_single():
     assert True
