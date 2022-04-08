27c7896efa4853bd3911f68754e49348ece23c8d
Shivek Chhabra <sca320@sfu.ca>
2022-04-07 20:26:45 -0700

    actions 13

--- c756-exer/leaderboard_local/test_app.py
+++ c756-exer/leaderboard_local/test_app.py
@@ -3,11 +3,11 @@ import requests
 
 class TestStringMethods(unittest.TestCase):
     def test_read(self):
-        response = requests.get("http://localhost:5000/api/v1/leaderboard/")
+        response = requests.get("http://0.0.0.0:5000/api/v1/leaderboard/")
         assert response.status_code == 200
 
     def test_read_single(self):
-        response = requests.get("http://localhost:5000/api/v1/leaderboard/33dc8408-be2b-410b-b12a-f48efdb6f15f")
+        response = requests.get("http://0.0.0.0:5000/api/v1/leaderboard/33dc8408-be2b-410b-b12a-f48efdb6f15f")
         assert response.json()['Item'] == {'Artist': 'Ed Sheeran', 'SongTitle': 'Shivers', 'genre': 'pop', 'music_id': '33dc8408-be2b-410b-b12a-f48efdb6f15f', 'upvotes': '26'}
 
 
