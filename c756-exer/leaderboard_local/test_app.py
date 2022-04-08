f1bb921a3c472fa54f4833cb63ee16d41e900bcb
Shivek Chhabra <sca320@sfu.ca>
2022-04-07 21:46:54 -0700

    actions 12

--- c756-exer/leaderboard_local/test_app.py
+++ c756-exer/leaderboard_local/test_app.py
@@ -1,20 +1,20 @@
-# import unittest
-# import requests
-#
-# class TestStringMethods(unittest.TestCase):
-#     def test_read(self):
-#         print("here")
-#         response = requests.get("http://0.0.0.0:5000/api/v1/leaderboard/")
-#         print("there")
-#         assert response.status_code == 200
-#
-#     def test_read_single(self):
-#         response = requests.get("http://0.0.0.0:5000/api/v1/leaderboard/33dc8408-be2b-410b-b12a-f48efdb6f15f")
-#         assert response.json()['Item'] == {'Artist': 'Ed Sheeran', 'SongTitle': 'Shivers', 'genre': 'pop', 'music_id': '33dc8408-be2b-410b-b12a-f48efdb6f15f', 'upvotes': '26'}
-#
-#
+import unittest
+import requests
+
+class TestStringMethods(unittest.TestCase):
+    def test_read(self):
+        print("here")
+        response = requests.get("http://0.0.0.0:5000/api/v1/leaderboard/")
+        print("there")
+        assert response.status_code == 200
+
+    def test_read_single(self):
+        response = requests.get("http://0.0.0.0:5000/api/v1/leaderboard/33dc8408-be2b-410b-b12a-f48efdb6f15f")
+        assert response.json()['Item'] == {'Artist': 'Ed Sheeran', 'SongTitle': 'Shivers', 'genre': 'pop', 'music_id': '33dc8408-be2b-410b-b12a-f48efdb6f15f', 'upvotes': '26'}
+
+
 if __name__ == '__main__':
     print("hello")
-    # unittest.main()
+    unittest.main()
 
 # print("hello")
