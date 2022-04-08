b91de1bd8ff171ac5727a52594c02d670e4a1a06
Akshita2810 <97658831+Akshita2810@users.noreply.github.com>
2022-04-08 15:43:01 -0700

    Testing actions for all endpoints

--- c756-exer/leaderboard/test_app.py
+++ c756-exer/leaderboard/test_app.py
@@ -32,3 +32,45 @@ def test_upvote():
             flag = 1
     if flag == 0:
         assert False
+
+def test_downvote():
+    test_id = '22e47f97-11ca-4c3c-8e77-f3068fddaf6e'
+    orig_votes = ''
+    before_downvote = requests.get("http://44.238.226.5:80/api/v1/leaderboard/", auth=(username, password))
+
+    op = before_downvote.json()
+    for i in op['Items']:
+        if i['music_id'] == test_id:
+            orig_votes = i['Votes']
+            break
+    response = requests.post("http://44.238.226.5:80/api/v1/leaderboard/downvote/22e47f97-11ca-4c3c-8e77-f3068fddaf6e",
+                             auth=(username, password))
+
+    after_downvote = requests.get("http://44.238.226.5:80/api/v1/leaderboard/", auth=(username, password))
+    op2 = after_downvote.json()
+    flag = 0
+    for i in op2['Items']:
+        if i['music_id'] == test_id and i['Votes'] == str(int(orig_votes) - 1):
+            assert True
+            flag = 1
+    if flag == 0:
+        assert False
+
+def test_tabletopper():
+    response = requests.get("http://44.238.226.5:80/api/v1/leaderboard/tabletopper", auth=(username, password))
+    assert response.status_code == 200
+
+def test_genre():
+    genre = 'pop'
+    lists = []
+    flag = 0
+    response = requests.get("http://44.238.226.5:80/api/v1/leaderboard/pop", auth=(username, password))
+    result = response.json()
+    for i in result['Items']:
+        lists.append[i['Genre']]
+    pop = all(element == lists[0] for element in lists)
+    if pop and lists[0]==genre:
+        assert True
+        flag = 1
+    if flag == 0:
+        assert False
