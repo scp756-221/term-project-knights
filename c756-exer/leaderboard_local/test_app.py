b51c8b4d2c4030ea6c410d2c35be3d52b4b02252
Shivek Chhabra <sca320@sfu.ca>
2022-04-08 14:14:38 -0700

    actions 6

--- c756-exer/leaderboard_local/test_app.py
+++ c756-exer/leaderboard_local/test_app.py
@@ -13,7 +13,4 @@ def test_read_single():
     genre = 'pop'
     music_id = '28'
     response = put_music(music_id=music_id, artist=Artist, SongTitle=SongTitle, upvotes=upvotes, genre=genre)
-    assert response.status_code==200
-    # response = requests.get("http://0.0.0.0:5000/api/v1/leaderboard/33dc8408-be2b-410b-b12a-f48efdb6f15f")
-    # assert response.json()['Item'] == {'Artist': 'Ed Sheeran', 'SongTitle': 'Shivers', 'genre': 'pop', 'music_id': '33dc8408-be2b-410b-b12a-f48efdb6f15f', 'upvotes': '26'}
-
+    assert response['ResponseMetadata']['HTTPStatusCode']==200
